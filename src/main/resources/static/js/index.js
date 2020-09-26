e75453876c1460ccdc0fcb55d94f79721db96a48
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-26 14:16:56 +0800

    展示页面

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,31 @@
 layui.define(['layer'], function (exports) {
 
+    const $ = layui.$,
+        layer = layui.layer;
+
     const index = {
         hello: function (str) {
             layer.msg('Hello, 欢迎使用 ' + (str || 'quartz'));
+        },
+
+        /**
+         * 展示页面
+         * @param target 需要展示URL的位置
+         * @param defaultHash 默认 hash 值
+         * @param otherNav 其他导航栏，不为空
+         * @param thisNav 当前导航栏，不为空
+         * @param thisClass 当前 class，默认值：nav-this，可为空
+         */
+        showNav: function (target, defaultHash, otherNav, thisNav, thisClass) {
+            const currentHash = window.location.hash.slice(1) || defaultHash || '/'; // 当前 hash
+            thisClass = thisClass || 'nav-this' // this a class
+            $(otherNav).removeClass(thisClass) // 移除 this
+            $(thisNav).addClass(thisClass) // 添加 this
+            $.get(currentHash).done(function (data) { // 请求 hash
+                $(target).html(data) // 页面显示
+            })
         }
+
     };
 
     // 输出 index 接口
