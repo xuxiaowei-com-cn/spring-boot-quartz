23848f7db595e913abedd4d6eaadbc5b5639d0f8
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-09-26 17:30:26 +0800

    分页

--- src/main/java/cn/com/xuxiaowei/controller/QrtzLocksRestController.java
+++ src/main/java/cn/com/xuxiaowei/controller/QrtzLocksRestController.java
@@ -3,6 +3,7 @@ package cn.com.xuxiaowei.controller;
 
 import cn.com.xuxiaowei.entity.QrtzLocks;
 import cn.com.xuxiaowei.service.IQrtzLocksService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static cn.com.xuxiaowei.util.Constants.*;
@@ -39,17 +39,19 @@ public class QrtzLocksRestController {
      *
      * @param request  请求
      * @param response 响应
+     * @param current  当前页，默认 1
+     * @param size     每页显示条数，默认 10
      * @return 返回 数据
      */
     @RequestMapping("/list")
-    public Map<String, Object> list(HttpServletRequest request, HttpServletResponse response) {
+    public Map<String, Object> list(HttpServletRequest request, HttpServletResponse response, Long current, Long size) {
         Map<String, Object> map = new HashMap<>(4);
         Map<String, Object> data = new HashMap<>(4);
 
         map.put(DATA, data);
 
-        List<QrtzLocks> qrtzLocksList = iQrtzLocksService.list();
-        data.put("qrtzLocksList", qrtzLocksList);
+        Page<QrtzLocks> qrtzLocksPage = iQrtzLocksService.page(new Page<>(current == null ? 1 : current, size == null ? 10 : size));
+        data.put("qrtzLocksPage", qrtzLocksPage);
 
         map.put(CODE, CODE_OK);
         map.put(MSG, "请求成功");
