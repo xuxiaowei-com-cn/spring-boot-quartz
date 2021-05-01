c169b159b13f7af1c5f55041655a86d78a1af2ad
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-05-01 16:33:27 +0800

    注释

--- src/main/java/cn/com/xuxiaowei/SpringBootQuartzApplication.java
+++ src/main/java/cn/com/xuxiaowei/SpringBootQuartzApplication.java
@@ -1,9 +1,13 @@
 package cn.com.xuxiaowei;
 
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
 import org.quartz.impl.jdbcjobstore.DriverDelegate;
 import org.quartz.impl.jdbcjobstore.StdJDBCDelegate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
 
 /**
  * 程序执行入口
@@ -11,6 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author xuxiaowei
  * @see DriverDelegate 操作数据接口
  * @see StdJDBCDelegate 对于完全兼容JDBC的驱动程序，操作数据实现类
+ * {@link StdJDBCDelegate#insertJobDetail(Connection, JobDetail)} 插入
+ * {@link StdJDBCDelegate#serializeJobData(JobDataMap)} 序列化
  */
 @SpringBootApplication
 public class SpringBootQuartzApplication {
