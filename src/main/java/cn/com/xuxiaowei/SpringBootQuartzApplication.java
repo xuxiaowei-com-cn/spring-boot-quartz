eabcccf61d51203c2bc04e743bbdbbf6c265f12d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-05-01 16:26:03 +0800

    操作数据接口

--- src/main/java/cn/com/xuxiaowei/SpringBootQuartzApplication.java
+++ src/main/java/cn/com/xuxiaowei/SpringBootQuartzApplication.java
@@ -1,5 +1,7 @@
 package cn.com.xuxiaowei;
 
+import org.quartz.impl.jdbcjobstore.DriverDelegate;
+import org.quartz.impl.jdbcjobstore.StdJDBCDelegate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 程序执行入口
  *
  * @author xuxiaowei
+ * @see DriverDelegate 操作数据接口
+ * @see StdJDBCDelegate 对于完全兼容JDBC的驱动程序，操作数据实现类
  */
 @SpringBootApplication
 public class SpringBootQuartzApplication {
